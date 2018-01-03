5b00d87644f0510d7609b4a6934f6e887c62d957
BottCode <mattiabottaro9@gmail.com>
2018-01-03 23:51:45 +0100

    Add method choices

--- theCountdownProblem/countdown.hs
+++ theCountdownProblem/countdown.hs
@@ -22,8 +22,7 @@ data Expr = Val Int | App Op Expr Expr
 
 instance Show Expr where
     show (Val n) = show n
-    show (App o l r) = brak l ++ show o ++ brak r
-    where
+    show (App o l r) = brak l ++ show o ++ brak r where
         brak (Val n) = show n
         brak e = "(" ++ show e ++ ")"
 
@@ -31,11 +30,11 @@ values :: Expr -> [Int]
 values (Val n) = [n | n > 0]
 values (App _ l r) = values l ++ values r
 
-eval :: -> Expr -> [Int]
+eval :: Expr -> [Int]
 eval (Val n) = [n | n > 0]
 eval (App o l r) = [apply o x y | x <- eval l, y <- eval r, valid o x y]
 
-subs [a] -> [[a]]
+subs :: [a] -> [[a]]
 subs [] = [[]]
 subs (x:xs) = yss ++ map (x:) yss
     where yss = subs xs
@@ -47,3 +46,6 @@ interleave x (y:ys) = (x:y:ys) : map (y:) (interleave x ys)
 perms :: [a] -> [[a]]
 perms [] = [[]]
 perms (x:xs) = concat (map (interleave x) (perms xs))
+
+choices :: [a] -> [[a]]
+choices = concat . map perms . subs
