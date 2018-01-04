2971d739bcc11c165a43fc1b978c0827cbf20bfa
BottCode <mattiabottaro9@gmail.com>
2018-01-04 13:50:49 +0100

    Hangman done

--- hangman.hs
+++ hangman.hs
@@ -1,3 +1,4 @@
+import System.IO
 
 getLine :: IO String
 getLine = 
@@ -26,4 +27,40 @@ strlen =
        Main.putStr "The string has "
        Main.putStr (show (length xs))
        Main.putStrLn " characters"
-    
+
+hangman :: IO ()
+hangman = 
+    do Main.putStrLn "Think of a word:"
+       word <- sgetLine
+       Main.putStrLn "Try to guess it:"
+       play word
+
+sgetLine :: IO String
+sgetLine = 
+    do x <- getCh
+       if x == '\n' 
+          then do putChar x
+                  return []
+          else do putChar '-'
+                  xs <- sgetLine
+                  return (x:xs)
+
+getCh :: IO Char
+getCh = 
+    do hSetEcho stdin False
+       x <- getChar
+       hSetEcho stdin True
+       return x
+
+play :: String -> IO ()
+play word =
+    do Main.putStr "? "
+       guess <- Main.getLine
+       if guess == word
+            then Main.putStrLn "You got it!"
+       else
+            do Main.putStrLn (match word guess)
+               play word
+
+match :: String -> String -> String
+match xs ys = [if elem x ys then x else '-' | x <- xs]
